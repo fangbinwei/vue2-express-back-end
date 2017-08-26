6662db95283f1875f0602e9533d3683e6d962f1c
FangBinwei <fangbinwei@qq.com>
2017-08-26 22:41:01 +0800

    在article文档中加入未经过解析的markdown输入内容

---
+++ server/models/db.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose')
+
+const article = new mongoose.Schema({
+    title: String,
+    createTime: Date,
+    modifyTime: {
+        type: Date,
+        default: Date.now
+    },
+    abstract: String,
+    content: String,
+    rawContent: String,
+    draft: String,
+    category: String,
+    visit: {
+        type: Number,
+        default: 0
+    }
+
+})
+
+const user = new mongoose.Schema({
+    userName: String,
+    password: String
+})
+
+let models = {
+    article: mongoose.model('article', article),
+    user: mongoose.model('user', user)
+}
+
+module.exports = models
