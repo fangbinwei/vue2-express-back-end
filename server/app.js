52bbb03d32cce7580c6be2832a44cc5c72964dea
FangBinwei <fangbinwei@qq.com>
2017-09-01 22:08:28 +0800

    取消全局拦截,改用路由拦截

--- server/app.js
+++ server/app.js
@@ -8,8 +8,6 @@ var bodyParser = require('body-parser');
 var index = require('./routes/index');
 var article = require('./routes/article');
 var user = require('./routes/user')
-var jwt = require('jsonwebtoken')
-var secret = 'fang-SleepWalker'
 
 var app = express();
 
@@ -31,57 +29,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 //     res.header('Access-Control-Allow-Headers', 'Content-Type, access-token, Content-Length, Authorization, Accept, X-Requested-With , yourHeaderField')
 //     res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS')
 //     next()
-// })
-app.use((req, res, next) => {
-    let token = req.headers['access-token']
-    // 验证header中的token是否过期 放弃验证cookies
-    // if (req.cookies['User-Token']) {
-    // console.log('cookies', req.cookies['User-Token'])
-    // console.log('token', token)
-    if (token) {
-        // TODO 验证token是否过期
-        jwt.verify(token, secret, function (err, decoded) {
-            // token过期
-            if (!decoded) {
-                res.json({
-                    status: '02',
-                    msg: 'token过期,请重新登入',
-                    result: ''
-                })
-            } else {
-                // console.log('token 未过期')
-                next()
-            }
-        })
-    } else {
-        // console.log('orginalUrl', req.originalUrl)
-        // 如果是login发送的请求,可以通过
-        console.log('original URL', req.originalUrl)
-        if (req.originalUrl === '/api/login'){
-            next()
-        } else {
-            res.json({
-                status: '01',
-                msg: '未登入, 这条信息应该不会出现',
-                result: ''
-            })
-        }
-    }
-})
-// app.use((req, res, next) => {
-//     if(req.cookies.userId) {
-//       next()
-//     } else {
-//         if (req.originalUrl === '/api/login' || req.originalUrl === '/api/logout') {
-//             next()
-//         } else {
-//             res.json({
-//                 status: '10001',
-//                 msg: '当前未登入',
-//                 result: ''
-//             })
-//         }
-//     }
 // })
 // app.use('/', index);
 app.use('/api', user);
