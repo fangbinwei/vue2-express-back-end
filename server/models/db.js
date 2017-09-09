7019f94f3ffdf2aea6860d1a0046b6ec424831e3
FangBinwei <fangbinwei@qq.com>
2017-09-10 00:21:39 +0800

    评论的模型

--- server/models/db.js
+++ server/models/db.js
@@ -19,6 +19,23 @@ const article = new mongoose.Schema({
 
 })
 
+const comment = new mongoose.Schema({
+    articleId: String,
+    replyId: String,
+    //回复人姓名
+    name: String,
+    replyTime: {
+        type: Date,
+        default: Date.now
+    },
+    content: String,
+    // 评论是否通过审核
+    state: {
+        type: Boolean,
+        default: false
+    }
+})
+
 const user = new mongoose.Schema({
     userName: String,
     password: String
@@ -26,7 +43,8 @@ const user = new mongoose.Schema({
 
 let models = {
     article: mongoose.model('article', article),
-    user: mongoose.model('user', user)
+    user: mongoose.model('user', user),
+    comment: mongoose.model('comment', comment)
 }
 
 module.exports = models
